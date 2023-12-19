f24aa6afa3d91b7d153a1588f79864a80817db29
José Andrés Murillo Manco <jose.murillo@sqasa.co>
2023-12-19 16:28:28 -0500

    commit 2 para el reto

---
+++ src/main/java/com/demoblaze/UI/NuevaClassUI.java
@@ -0,0 +1,10 @@
+package com.demoblaze.UI;
+
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class NuevaClassUI {
+    public static final Target TXT_EMAIL= Target.the( "text input email" )
+            .located( By.xpath("//a[@class='carousel-control-next']") );
+    
+}
